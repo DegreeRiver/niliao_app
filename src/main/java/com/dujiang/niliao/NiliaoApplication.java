9cfd3983ae1e796137e327955c1f480a32076407
dubaolong <649001717@qq.com>
2018-10-06 10:03:35 +0800

    匿liao项目初始化

---
+++ src/main/java/com/dujiang/niliao/NiliaoApplication.java
@@ -0,0 +1,12 @@
+package com.dujiang.niliao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiliaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiliaoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dujiang/niliao/NiliaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dujiang.niliao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NiliaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
